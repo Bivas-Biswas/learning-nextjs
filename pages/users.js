407c771eba8b052b703f5b52f0f758d47bc64bc1
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-22 14:22:12 +0530

    static generation with data fetching

---
+++ pages/users.js
@@ -0,0 +1,37 @@
+function UserList({users}) {
+
+    return (
+        <>
+        <h1>List of Users</h1>
+            {
+                users.map((user) =>{
+                    return (
+                        <div key={user.id}>
+                            <p>{user.name}</p>
+                            <p>{user.email}</p>
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default UserList
+
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    console.log(data)
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            users: data
+        }
+    }
+}
