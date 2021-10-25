5a51080c1143292b85f21a8bdd94f449e12a23e5
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-25 14:11:57 +0530

    getServerSideProps context parameter keys

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -1,10 +1,10 @@
-function ArticleListByCategory({articles, category}) {
+function ArticleListByCategory({ articles, category }) {
 
     return (
         <>
             <h1>Showing news for category <i>{category}</i></h1>
             {
-                articles.map(article=>(
+                articles.map(article => (
                     <div key={article.id}>
                         <h2>
                             {article.id} {article.title}
@@ -21,10 +21,13 @@ function ArticleListByCategory({articles, category}) {
 export default ArticleListByCategory
 
 export async function getServerSideProps(context) {
-    const { params} = context
+    const { params, req, res, query } = context
+    console.log(req.headers.cookie)
+    console.log(query)
+    res.setHeader('set-cookie', ['name=Bivas'])
     const { category } = params
-    const res = await fetch(`http://localhost:4000/news?category=${category}`)
-    const data = await res.json()
+    const response = await fetch(`http://localhost:4000/news?category=${category}`)
+    const data = await response.json()
 
     return {
         props: {
