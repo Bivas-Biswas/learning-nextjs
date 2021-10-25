e8515eafe08a6a0679dae1e828a15b9722704abb
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-25 14:33:12 +0530

    client side data fetching

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -28,7 +28,7 @@ export async function getServerSideProps(context) {
     const { category } = params
     const response = await fetch(`http://localhost:4000/news?category=${category}`)
     const data = await response.json()
-
+    console.log(`Pre-rendering news articles for category ${category}`)
     return {
         props: {
             articles: data,
