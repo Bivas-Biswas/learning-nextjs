3222ba23ff84e720c5de8f6b5e29a49533382cf1
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-27 14:01:58 +0530

    Head Component

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,28 @@ import '../styles/globals.css'
 import '../styles/layout.css'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
 
-    if(Component.getLayout){
-        return Component.getLayout(<Component {...pageProps}/>)
+    if (Component.getLayout) {
+        return Component.getLayout(
+            <>
+                <Head>
+                    <title>Global Head tag</title>
+                    <meta name={'description'} content={'Test Global head tag'}/>
+                </Head>
+                <Component {...pageProps}/>
+            </>
+        )
     }
 
     return (
         <>
+            <Head>
+                <title>Global Head tag</title>
+                <meta name={'description'} content={'Test Global head tag'}/>
+            </Head>
             <Header/>
             <Component {...pageProps} />
             <Footer/>
--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,23 @@
 import Footer from "../components/Footer";
+// import Head from "next/head";
 
 function About() {
-    return <h1 className={'content'}>About</h1>
+    return (
+        <>
+            {/*<Head>*/}
+            {/*    <title>About The Site</title>*/}
+            {/*    <meta name={'description'} content={'learing nextjs'}/>*/}
+            {/*</Head>*/}
+            <h1 className={'content'}>About</h1>
+
+        </>
+    )
 }
 
 export default About
 
 
-About.getLayout = function PageLayout(page){
+About.getLayout = function PageLayout(page) {
     return (
         <>
             {page}
