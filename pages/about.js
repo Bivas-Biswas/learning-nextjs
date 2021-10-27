2541975176431b7ca5949543cf9f4c912156a756
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-27 12:54:52 +0530

    component level styles, css module

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import styles from '../styles/About.module.css'
+
 function About() {
     return (
-        <>
+        <div className={styles.highlight}>
             <h2>About Page</h2>
             <button className={'btn btn-primary'}>Primary</button>
-        </>
+        </div>
     )
 
 }
