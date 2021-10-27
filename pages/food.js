3f858ecafd7ea1b5d629cab73dd5299af0e46e75
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-27 20:42:38 +0530

    static html support

--- pages/food.js
+++ pages/food.js
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import img from '../public/1.jpg'
+
 
 function FoodsPage() {
     return (
         <div>
-            <Image src={img} quality={10} placeholder={'blur'} alt={'image'} width={'280px'} height={'420px'}/>
+            <img src={`/1.jpg`} placeholder={'blur'} alt={'image'} width={'280px'} height={'420px'}/>
             {
                 [...Array(5)].map((_, index) => (
                     <div key={index}>
-                        <Image  src={`/${index + 1}.jpg`} alt={'image'} width={'280px'} height={'420px'}/>
+                        <img  src={`/${index + 1}.jpg`} alt={'image'} width={'280px'} height={'420px'}/>
                     </div>
                 ))
             }
