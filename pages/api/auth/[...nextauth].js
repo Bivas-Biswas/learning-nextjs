8373f7e394268b75f47b639f9e87c08dc75bb18e
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-28 13:58:04 +0530

    callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,4 +15,34 @@ export default NextAuth({
     jwt: {
         secret: 'afjsdaj1212jsaf'
     },
+    callbacks: {
+        async jwt(token, user, account) {
+            if (user) {
+                token.id = user.id
+                return token
+            }
+            return token
+        },
+        async session(session, token) {
+            session.user.id = token.id
+            return session
+        },
+        signIn: async (profile, account, metadata) => {
+            // console.info('we are here to see the callback\nP\nP');
+            // console.log(profile, 'is the profile');
+            // console.log(account, 'is the account');
+            // console.log(metadata, 'is the metadata');
+            const res = await fetch('https://api.github.com/user/emails', {
+                headers: {
+                    'Authorization': `token ${account.accessToken}`
+                }
+            })
+            const emails = await res.json()
+            if (!emails || emails.length === 0) {
+                return
+            }
+            const sortedEmails = emails.sort((a, b) => b.primary - a.primary)
+            profile.email = sortedEmails[0].email
+        },
+    }
 })
