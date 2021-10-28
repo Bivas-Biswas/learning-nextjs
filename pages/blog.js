c3ec6962c2a93a592b750065069eb9d94f9c0aac
BivasBiswas <bivasbiswas1999@gmail.com>
2021-10-28 10:38:19 +0530

    server side authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,25 @@
-function Blog() {
+import { getSession, useSession } from "next-auth/client";
+
+function Blog({ data }) {
+    const [session] = useSession()
+    console.log({ session })
     return (
         <>
-        <h1>
-            Blog Page
-        </h1>
+            <h1>
+                Blog Page - {data}
+            </h1>
         </>
     )
 }
 
 export default Blog
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+    return {
+        props: {
+            session,
+            data: session ? "List of 100 personalized blogs" : 'list of free blogs',
+        }
+    }
+}
